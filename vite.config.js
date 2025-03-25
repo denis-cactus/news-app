2459fc40757acf4aedbd8d269b26e4c6139d76e1
denis-cactus <den.waves@gmail.com>
2025-03-25 15:54:37 +0200

    upd

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 
 export default defineConfig({
-  define: {
-    "process.env": process.env,
-  },
+	base: "/gallery-app/",
 });
